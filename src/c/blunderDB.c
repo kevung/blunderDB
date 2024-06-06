f42ad30b9ead09943daf23ce3168704795a2633a
Kévin UNGER <kevin.unger@protonmail.com>
2024-06-06 22:18:42 +0200

    draw rectangle

--- src/c/blunderDB.c
+++ src/c/blunderDB.c
@@ -1,15 +1,52 @@
 #include <stdlib.h>
 #include <iup.h>
 
+/************************ Interface ***********************/
+
+#define DEFAULT_SIZE "400x300"
+
+
+static int canvas_action(Ihandle *ih)
+{
+    int i, w, h;
+
+    IupDrawBegin(ih);
+
+    IupDrawGetSize(ih, &w, &h);
+
+    IupSetAttribute(ih, "DRAWCOLOR", "252 186 3");
+    IupSetAttribute(ih, "DRAWSTYLE", "FILL");
+    IupDrawRectangle(ih, 0, 0, w-1, h-1);
+
+
+    IupDrawEnd(ih);
+    return IUP_DEFAULT;
+}
+
+
+/************************ Main ****************************/
 int main(int argc, char **argv)
 {
-  Ihandle *dlg, *label;
+  Ihandle *dlg, *hbox, *vbox, *label;
+  Ihandle *canvas;
 
   IupOpen(&argc, &argv);
 
-  label =  IupLabel("Hello world from IUP.");
-  dlg = IupDialog(IupVbox(label, NULL));
-  IupSetAttribute(dlg, "TITLE", "Hello World 2");
+  canvas = IupCanvas(NULL);
+  IupSetAttribute(canvas, "NAME", "CANVAS");
+  IupSetAttribute(canvas, "EXPAND", "YES");
+
+  vbox = IupVbox(canvas, NULL);
+  IupSetAttribute(vbox, "NMARGIN", "10x10");
+  IupSetAttribute(vbox, "GAP", "10");
+
+  dlg = IupDialog(vbox);
+  IupSetAttribute(dlg, "TITLE", "BlunderDB");
+  IupSetAttribute(dlg, "SIZE", DEFAULT_SIZE);
+
+  /* Registers callbacks */
+  IupSetCallback(canvas, "ACTION", (Icallback)canvas_action);
+
 
   IupShowXY(dlg, IUP_CENTER, IUP_CENTER);
 
