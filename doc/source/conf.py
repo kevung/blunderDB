dbc35d11141692e38d949931c8c1e1e643932e35
Kévin Unger <kevin.unger@proton.me>
2024-09-16 02:42:04 +0200

    conf change theme and other options

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'blunderdb'
+project = 'blunderDB'
 copyright = '2024, Kevin UNGER <blunderdb@proton.me>'
 author = 'Kevin UNGER <blunderdb@proton.me>'
 release = '0.1.0'
@@ -14,7 +14,9 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+        'sphinx_rtd_theme',
+        ]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +26,7 @@ language = 'fr'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'traditional'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
+html_show_sphinx = False
+html_show_sourcelink = False
