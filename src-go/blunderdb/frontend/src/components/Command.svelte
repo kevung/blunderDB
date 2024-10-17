11f87663e71e0e9bd068e6643f466aaf78757d6f
Kévin Unger <kevin.unger@proton.me>
2024-10-17 02:01:28 +0200

    go: command harmonize look

--- src-go/blunderdb/frontend/src/components/Command.svelte
+++ src-go/blunderdb/frontend/src/components/Command.svelte
@@ -36,20 +36,24 @@
          bind:this={inputEl}
          bind:value={text}
          placeholder=" Type your command here. "
-         style="position: fixed; top: 10px; left: 50%; transform: translateX(-50%); z-index: 1000;"
          on:keydown={handleKeyDown}
          />
 {/if}
 
 <style>
-   input {
-      width: 90%;
-      padding: 8px;
-      border: 1px solid rgba(0, 0, 0, 0.1); /* Subtle border */
-      border-radius: 4px;
-      box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-      outline: none;
-      background-color: white; /* Ensure background is opaque */
-      font-size: 21px;
-   }
+    input {
+        position: fixed;
+        top: 350px;
+        left: 50%;
+        transform: translateX(-50%);
+        z-index: 1000;
+        width: 70%;
+        padding: 8px;
+        border: 1px solid rgba(0, 0, 0, 0.3); /* Subtle border */
+        border-radius: 1px;
+        box-shadow: 0 2px 10px rgba(0, 0, 0, 0);
+        outline: none;
+        background-color: white; /* Ensure background is opaque */
+        font-size: 18px;
+    }
 </style>
