dd66d5570279eb4c6d52cc160b5840fd9cb5f901
Kévin Unger <kevin.unger@proton.me>
2024-11-18 23:25:06 +0100

    analysisStore update store structure

--- src-go/blunderdb/frontend/src/stores/analysisStore.js
+++ src-go/blunderdb/frontend/src/stores/analysisStore.js
@@ -1,13 +1,33 @@
 import { writable } from 'svelte/store';
 
+
 export const analysisStore = writable({
     positionId: null,
-    xgid: '',
     player1: '',
     player2: '',
-    extremeGammonVersion: '',
-    analysisType: '',
-    doublingCubeAnalysis: null,
-    checkerAnalysis: [],
+    xgid: '', // The XGID string
+    analysisType: '', // Either "DoublingCube" or "CheckerMove"
+    analysisEngineVersion: '', // The version of the analysis engine (e.g., eXtreme Gammon)
+    checkerAnalysis: [], // Array to hold checker move analysis
+    doublingCubeAnalysis: {
+        analysisDepth: '',
+        playerWinChances: 0,
+        playerGammonChances: 0,
+        playerBackgammonChances: 0,
+        opponentWinChances: 0,
+        opponentGammonChances: 0,
+        opponentBackgammonChances: 0,
+        cubelessNoDoubleEquity: 0,
+        cubelessDoubleEquity: 0,
+        cubefulNoDoubleEquity: 0,
+        cubefulNoDoubleError: 0,
+        cubefulDoubleTakeEquity: 0,
+        cubefulDoubleTakeError: 0,
+        cubefulDoublePassEquity: 0,
+        cubefulDoublePassError: 0,
+        bestCubeAction: '',
+        wrongPassPercentage: 0,
+        wrongTakePercentage: 0,
+    },
 });
 
