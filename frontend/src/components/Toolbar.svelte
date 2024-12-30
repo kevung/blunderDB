c39151626a127ff146f740ebe3b36280eaa864dd
Kévin Unger <kevin.unger@proton.me>
2024-12-30 01:14:03 +0100

    fix next position button look

--- frontend/src/components/Toolbar.svelte
+++ frontend/src/components/Toolbar.svelte
@@ -217,8 +217,8 @@
     </button>
 
     <button on:click|stopPropagation={handleNextPosition} aria-label="Next Position" title="Next Position (Right, j)" disabled={statusBarMode === 'EDIT' || !databasePath}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
         </svg>
     </button>
 
