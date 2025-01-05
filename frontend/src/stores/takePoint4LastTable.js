f57d928618e48bed1c1168ca4f6a254c2027bb09
Kévin UNGER <kevin.unger@protonmail.com>
2025-01-05 20:50:20 +0100

    fix bug wrong display due to use of svelte writable

--- frontend/src/stores/takePoint4LastTable.js
+++ frontend/src/stores/takePoint4LastTable.js
@@ -1,6 +1,4 @@
-import { writable } from 'svelte/store';
-
-export const takePoint4LastTable = writable([
+export const takePoint4LastTable = [
   [25, 40, 33, 29, 30, 33, 32],
   [19, 33, 30, 25, 26, 29, 29],
   [21, 31, 28, 26, 27, 28, 28],
@@ -8,4 +6,4 @@ export const takePoint4LastTable = writable([
   [19, 27, 26, 26, 26, 27, 27],
   [16, 25, 25, 25, 25, 26, 26],
   [16, 23, 23, 24, 25, 26, 26]
-]);
+];
