9933f906782fe2e408fd78878a4ed9fc50ad1817
Kévin UNGER <kevin.unger@protonmail.com>
2025-01-06 12:23:36 +0100

    bump version to 0.2.1 in documentation and meta store

--- frontend/src/stores/metaStore.js
+++ frontend/src/stores/metaStore.js
@@ -1,5 +1,5 @@
 import { writable } from 'svelte/store';
 
 export const metaStore = writable({
-    applicationVersion: '0.2.0' // Example application version
+    applicationVersion: '0.2.1',
 });
