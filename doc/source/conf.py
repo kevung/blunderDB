468bf76f219fee9007e3d4cf33366a3a5d4615af
Kévin UNGER <kevin.unger@onera.fr>
2025-02-06 10:21:24 +0100

    doc add custom css for tables

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -30,6 +30,7 @@ html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
 html_css_files = [
         'theme_overrides.css',
+        'custom.css',
          ]
 html_show_sphinx = False
 html_show_sourcelink = False
