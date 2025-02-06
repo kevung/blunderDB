03e32399834ba0b91f758dc1bb7841bb1e255c86
Kévin UNGER <kevin.unger@onera.fr>
2025-02-06 10:07:12 +0100

    doc: extend display one whole browser window

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -21,23 +21,20 @@ extensions = [
         'sphinxcontrib.youtube'
         ]
 
+language = 'fr'
 templates_path = ['_templates']
 locale_dirs = ['locale/']
 gettext_compact = False
-html_favicon = '_static/favicon.ico'
-html_logo = '_static/logo.png'
 exclude_patterns = []
-
-language = 'fr'
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
+html_css_files = [
+        'theme_overrides.css',
+         ]
 html_show_sphinx = False
 html_show_sourcelink = False
-
+html_favicon = '_static/favicon.ico'
+html_logo = '_static/logo.png'
 html_context = {
         'languages': [["en", "../en"], ["fr", "../fr"]]
         }
