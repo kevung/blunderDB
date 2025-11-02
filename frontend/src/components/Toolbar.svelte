0b0f37036b655fc5775994f9da5d91cd08f93bfa
Kévin Unger <kevin.unger@proton.me>
2025-11-02 04:26:01 +0100

    change import database button icon

--- frontend/src/components/Toolbar.svelte
+++ frontend/src/components/Toolbar.svelte
@@ -164,7 +164,7 @@
 
     <button on:click|stopPropagation={onImportDatabase} aria-label="Import Database" title="Import Database" disabled={!databasePath}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="m9 13.5 3 3m0 0 3-3m-3 3v-6m1.06-4.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15M9 12l3 3m0 0 3-3m-3 3V2.25" />
         </svg>
     </button>
 
